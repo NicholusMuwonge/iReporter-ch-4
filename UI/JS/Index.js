2e8b4732789c36b97e8f9d4313cc38ee818a7f7a
NicholusMuwonge <nicholusmuwonge@gmail.com>
2019-02-01 21:59:15 +0300

    adding javascript

--- UI/JS/Index.js
+++ UI/JS/Index.js
@@ -15,36 +15,37 @@ function login() {
                 },
                 cache : 'no-chache'
     }
-    fetch(route,options)
-    .then (res => res.json())
-    .then (response_object => {
-        if (response_object.error_message == "some fields are missing"){
-            return document.getElementById('error').innerHTML = "some fields \
-            are missing";
-        }
-        else if (response_object.error_message == 'Please use character strings'){
-            return document.getElementById('error').innerHTML = 'Please use\
-            character strings';
-        }
-        else if (response_object.error_message == 'Some fields have no data'){
-            return document.getElementById('error').innerHTML = 'Some fields\
-             have no data';
-        }
-        else if (response_object.error_message == 'User does not exist.'){
-            return document.getElementById('error').innerHTML = 'User does \
-            not exist.';
-        }
-        else if (response_object.record_type == "TRUE"){
-            token = response_object["access_token"];
-            tokenstorage.SetItem('token',token);
-            window.location.assign("https://ireporter-ui.herokuapp.com/UI/admin_dashboard");
-        }
+    return fetch(route,options)
+        .then (res => res.json())
+        .then (response_object => {
+            if (response_object.error_message == "some fields are missing"){
+                return document.getElementById('error').innerHTML = "some fields \
+                are missing";
+            }
+            else if (response_object.error_message == 'Please use character strings'){
+                return document.getElementById('error').innerHTML = 'Please use\
+                character strings';
+            }
+            else if (response_object.error_message == 'Some fields have no data'){
+                return document.getElementById('error').innerHTML = 'Some fields\
+                have no data';
+            }
+            else if (response_object.error_message == 'User does not exist.'){
+                return document.getElementById('error').innerHTML = 'User does \
+                not exist.';
+            }
+            else if (response_object.record_type == "TRUE"){
+                token = response_object["access_token"];
+                tokenstorage.SetItem('token',token);
+                window.location.assign("https://ireporter-ui.herokuapp.com/UI/admin_dashboard");
+            }
 
-        else{
-            token = response_object["access_token"];
-            tokenstorage.SetItem('token',token);
-            window.location.assign("https://ireporter-ui.herokuapp.com/iReporter/UI/active");
-        }
+            else{
+                token = response_object["access_token"];
+                tokenstorage.SetItem('token',token);
+                window.location.assign("https://ireporter-ui.herokuapp.com/iReporter/UI/active");
+            }
 
-    })
-}
+        })
+        .catch(error => console.log(error));
+    }
