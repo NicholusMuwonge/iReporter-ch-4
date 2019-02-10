370401d6d430e2a80d2975294859107f8f03a609
NicholusMuwonge <nicholusmuwonge@gmail.com>
2019-02-10 17:10:32 +0300

    update-record-first-push

--- JS/edit.js
+++ JS/edit.js
@@ -50,7 +50,7 @@ function edit_record(){
                                     <li> <input  type="text" value=""  id="update_record_geolocation" placeholder="update here .." ></li>
                                     <li id="record_type">${response_object['record_type']}</li>
                                     <li id ="status">${response_object['status']}</li>
-                                    <li id = "record_geolocation" >${response_object['record_geolocation']}</li>
+                                    <li id = "record_geolocation" >${record_geolocation}</li>
                                     <li id = "record_no" >${response_object['record_no']}</li>
                                     
                                 </h3>
