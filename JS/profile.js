3b91d3f1694c53b3927d589d76186ca3dcbd5871
NicholusMuwonge <nicholusmuwonge@gmail.com>
2019-02-15 15:00:49 +0300

    chore-get-one-person-records

--- JS/profile.js
+++ JS/profile.js
@@ -27,42 +27,42 @@ function get_one_person_records() {
     .then (res => res.json())
     .then (response_object => {
         console.log(response_object)
-        if (n in response_object){
+        if (response_object){
             output = ``
+            for(n in response_object){
+                output += `
+                <br><br>
+                <div class="four" id = "template">
+                <div class="four">
+                        <form style="background-image:url('images/paper.jpg')" >
+                                    <div class="five">
+                                    <img id="w" src="images/avatar1.png" alt="avatar"  >
+                                    <h2 id="user_id">${user_name}</h2>
+                                    <h1 id = "record_date">${response_object[n]['record_placement_date']}</h1><br><br>
+                                    <p2 id = "body">${response_object[n]['body']}</p2>
+                                    <br><br><br><br>
             
-            output += `
-            <br><br>
-            <div class="four" id = "template">
-            <div class="four">
-                    <form style="background-image:url('images/paper.jpg')" >
-                                <div class="five">
-                                <img id="w" src="images/avatar1.png" alt="avatar"  >
-                                <h2 id="user_id">${user_name}</h2>
-                                <h1 id = "record_date">${response_object[n]['record_placement_date']}</h1><br><br>
-                                <p2 id = "body">${response_object[n]['body']}</p2>
-                                <br><br><br><br>
-        
-                                <ul class='first'>
-                                <h3 id = "inside-bars">
-                                    <li id="Edit"> <input  type="text" value="" id="update_record_geolocation" placeholder="update here .." ></li>
-                                    <li id="record_type">${response_object[n]['record_type']}</li>
-                                    <li id ="status">${response_object[n]['status']}</li>
-                                    <li id = "record_geolocation" >${response_object[n]['record_geolocation']}</li>
-                                    <li id = "record_no" >${response_object[n]['record_no']}</li>
+                                    <ul class='first'>
+                                    <h3 id = "inside-bars">
+                                        <li id="Edit"> <input  type="text" value="" id="update_record_geolocation" placeholder="update here .." ></li>
+                                        <li id="record_type">${response_object[n]['record_type']}</li>
+                                        <li id ="status">${response_object[n]['status']}</li>
+                                        <li id = "record_geolocation" >${response_object[n]['record_geolocation']}</li>
+                                        <li id = "record_no" >${response_object[n]['record_no']}</li>
+                                        
+                                    </h3>
+                                    </ul>
                                     
-                                </h3>
-                                </ul>
+                                    <div >
+                                        <button class="e" ><a  >Edit</a></button>
+                                        <li id="del"> 🚮 </li>
+                                    </div>
                                 
-                                <div >
-                                    <button class="e" ><a  >Edit</a></button>
-                                    <li id="del"> 🚮 </li>
-                                </div>
-                            
-                        </form>
-                    </div><br><br>`;
-                    console.log(output);
-                    document.getElementById('fillin').innerHTML= output;
-        }
+                            </form>
+                        </div><br><br>`;
+                        console.log(output);
+                        document.getElementById('fillin').innerHTML= output;
+        }}
 
         else if(response_object === "Token has expired") {
             window.location.replace(
